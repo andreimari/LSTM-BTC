9eb0e1f74f1bc4fb8743ec4f44f9b495b188b963
andreimari <andreimari97@yahoo.com>
2019-06-12 22:49:13 +0300

    update

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
-app = Flask(__name__, template_folder='FinalProject/templates')
+app = Flask(__name__)
 
 @app.route('/')
 def upload_file1():
