b85977389b4337f32c87601c85902d0762885762
andreimari <andreimari97@yahoo.com>
2019-06-12 23:20:41 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request
 from werkzeug import secure_filename
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/upload')
 def upload_file1():
    return render_template('upload.html')
 	
-@app.route('forecasting-btc.herokuapp.com/uploader', methods = ['GET', 'POST'])
+@app.route('/forecasting-btc.herokuapp.com/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
       f = request.files['file']
