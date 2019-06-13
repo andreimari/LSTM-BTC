2dbf5d9be2b0ba3ea5c5bb90512d6940274d160e
andreimari <andreimari97@yahoo.com>
2019-06-13 16:20:13 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,42 @@
-from flask import Flask, render_template, request, send_file
+import os
+from flask import Flask, flash, render_template, request, send_file, redirect, url_for
 from flask_restful import Api, Resource, reqparse 
-
 from werkzeug import secure_filename
-app = Flask(__name__)
-api = Api(app) 
 
-@app.route('/home')
-def upload_file1():
-   return render_template('upload.html')
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['.json'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      print(f.filename)
-      return f.filename
-		
-if __name__ == '__main__':
-   app.run(host= '0.0.0.0', debug = True)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
