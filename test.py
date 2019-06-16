273d39415b863dfe232af3965abc18596a90e7a4
andreimari <andreimari97@yahoo.com>
2019-06-17 00:03:01 +0300

    changes

--- test.py
+++ test.py
@@ -4,8 +4,10 @@ Created on Sun Jun 16 23:24:21 2019
 
 @author: Andrei
 """
+import gunicorn
 import requests
 
 r = requests.post('http://localhost:5000/titanic-survival-classification-model/predict')
 print("Andrei")
 print(r.content)
+print (gunicorn.__version__)
