2ce53aae4747cf9546df35a3438cf4edf4dc656f
andreimari <andreimari97@yahoo.com>
2019-06-17 00:07:27 +0300

    asd

--- test.py
+++ test.py
@@ -4,10 +4,9 @@ Created on Sun Jun 16 23:24:21 2019
 
 @author: Andrei
 """
-import gunicorn
+
 import requests
 
-r = requests.post('http://localhost:5000/titanic-survival-classification-model/predict')
+r = requests.post('https://forecasting-btc.herokuapp.com/titanic-survival-classification-model/predict')
 print("Andrei")
 print(r.content)
-print (gunicorn.__version__)
